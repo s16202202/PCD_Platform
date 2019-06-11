bc7eb476923ee1d033704c71dbafd4824f4810b2
s16202202 <1045418720@qq.com>
2019-06-11 15:33:14 +0800

    first

---
+++ PCD_Platform/src/main/java/qaq/pcd/PcdPlatformApplication.java
@@ -0,0 +1,13 @@
+package qaq.pcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcdPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcdPlatformApplication.class, args);
+	}
+
+}
